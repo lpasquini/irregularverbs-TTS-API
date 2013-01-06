cd5cb47ba105e19a092bbe271c87eb1a9480cfcd
Oswaldo Rubio <osrufung@gmail.com>
2013-01-06 08:26:23 +0100

    Initial Commit

---
+++ aListOfVerbsManager/ALOVDocument.h
@@ -0,0 +1,13 @@
+//
+//  ALOVDocument.h
+//  aListOfVerbsManager
+//
+//  Created by Oswaldo Rubio on 06/01/13.
+//  Copyright (c) 2013 Oswaldo Rubio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ALOVDocument : NSDocument
+
+@end
---
+++ aListOfVerbsManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  aListOfVerbsManager
+//
+//  Created by Oswaldo Rubio on 06/01/13.
+//  Copyright (c) 2013 Oswaldo Rubio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
