00b7a41d63723a3c7e4acd48eb115c3a7cbac49c
Oswaldo Rubio <osrufung@gmail.com>
2013-02-28 17:39:53 +0100

    download mp3 sounds from TTS-api.com

--- aListOfVerbsManager/ALOVDocument.h
+++ aListOfVerbsManager/ALOVDocument.h
@@ -7,7 +7,24 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ALOVDocument : NSDocument
 
+@interface ALOVDocument : NSDocument<AVAudioPlayerDelegate>
+{
+ 
+    NSMutableArray *verbsItems;
+    IBOutlet NSTableView *itemTableView;
+    NSArray *keysOfVerbs;
+ 
+}
+@property (strong, nonatomic)  NSMutableDictionary *verbsSounds;
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
+@property (weak) IBOutlet NSButton *playButton;
+@property (weak) IBOutlet NSProgressIndicator *progressIndicator;
+- (IBAction)playSelected:(id)sender;
+@property (weak) IBOutlet NSButton *getSpeechButton;
+ 
+- (IBAction)getSpeech:(id)sender;
+-(IBAction)createNewItem:(id)sender;
 @end
--- aListOfVerbsManager/ALOVDocument.m
+++ aListOfVerbsManager/ALOVDocument.m
@@ -8,17 +8,26 @@
 
 #import "ALOVDocument.h"
 
+ 
 @implementation ALOVDocument
 
+#pragma mark Document overrides
 - (id)init
 {
     self = [super init];
     if (self) {
         // Add your subclass-specific initialization here.
+        keysOfVerbs = @[@"simple",@"past",@"participle"];
+         
     }
     return self;
 }
-
+-(NSMutableDictionary *)verbsSounds{
+    if(!_verbsSounds){
+        _verbsSounds = [[NSMutableDictionary alloc] init];
+    }
+    return _verbsSounds;
+}
 - (NSString *)windowNibName
 {
     // Override returning the nib file name of the document
@@ -39,21 +48,130 @@
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return nil;
+    if(!verbsItems){
+        verbsItems = [NSMutableArray array];
+        
+    }
+    NSData *data= [NSPropertyListSerialization dataWithPropertyList:verbsItems format:NSPropertyListXMLFormat_v1_0 options:0 error:outError];
+    return data;
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return YES;
+    
+    
+     verbsItems = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListMutableContainers format:NULL error:outError];
+    return (verbsItems !=nil);
 }
 
+-(NSMutableSet *) getSingularTextFromVerbs{
+    
+    NSMutableSet *returnSet = [[NSMutableSet alloc] init];
+    for(int row=0;row< [verbsItems count];row++){
+        for(int col =0;col <[keysOfVerbs count];col++){
+            NSString *valueString =  [[verbsItems objectAtIndex:row] objectForKey:keysOfVerbs[col]];
+            [returnSet addObject:valueString];
+            
+        }
+    }
+    return  returnSet;
+}
+
+#pragma mark - Actions
+
+#define SPEECH_API_URL @"http://tts-api.com/tts.mp3?q=%@"
+
+
+
+- (IBAction)playSelected:(id)sender {
+    if([itemTableView selectedRow] >= 0){
+    NSString *simpleString =   [[verbsItems objectAtIndex:[itemTableView selectedRow]] valueForKey:@"simple"];;
+    
+    NSError *error;
+        NSData *dataSound = [self.verbsSounds valueForKey:simpleString];
+        NSLog(@"Sound Data %@",dataSound);
+     self.audioPlayer = [[AVAudioPlayer alloc] initWithData:dataSound error:&error];
+     if(!error){
+         self.audioPlayer.delegate = self;
+         [self.audioPlayer prepareToPlay];
+         [self.audioPlayer play];
+     }
+     else{
+         NSLog(@"Error %@",[error localizedDescription]);
+     }
+    }
+}
+
+- (IBAction)getSpeech:(id)sender {
+    [self.getSpeechButton setEnabled:NO];
+    dispatch_queue_t downloadQueue = dispatch_queue_create("speech downloader", NULL);
+ 
+    NSSet *verbsToDownload = self.getSingularTextFromVerbs;
+    
+    [self.progressIndicator setMinValue:0];
+    [self.progressIndicator setMaxValue:[verbsToDownload count]];
+    
+    [self.progressIndicator startAnimation:self];
+    [self.progressIndicator  setHidden:NO];
+    
+    dispatch_async(downloadQueue, ^{
+        
+        
+        for(id verb in verbsToDownload){
+                NSString *formattedURL = [NSString stringWithFormat:SPEECH_API_URL,verb];
+            NSLog(@"verb %@",formattedURL);
+            NSURL *networkURL = [[NSURL alloc] initWithString:formattedURL];
+            NSData *speechData = [NSData dataWithContentsOfURL:networkURL];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.progressIndicator incrementBy:1];
+      
+                [self.verbsSounds setValue:speechData forKey:verb];
+           
+              
+            });
+   
+        }
+ 
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.progressIndicator stopAnimation:self];
+             [self.progressIndicator  setHidden:YES];
+              [self.getSpeechButton setEnabled:YES];
+           });
+        
+        
+    });
+}
+
+
+
+-(IBAction)createNewItem:(id)sender{
+    if(!verbsItems){
+        verbsItems = [NSMutableArray array];
+    }
+    [verbsItems addObject:@"New item"];
+    [itemTableView reloadData];
+    [self updateChangeCount:NSChangeDone];
+    
+    
+    
+}
+
+#pragma mark - Data Source methods
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView{
+    return [verbsItems count];
+}
+
+
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex{
+
+    NSUInteger index= [keysOfVerbs indexOfObject:[aTableColumn identifier]];
+ 
+    return [[verbsItems objectAtIndex:rowIndex] objectForKey:keysOfVerbs[index]];
+}
+- (void)tableView:(NSTableView *)aTableView setObjectValue:(id)anObject forTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex{
+    //[todoItems replaceObjectAtIndex:rowIndex withObject:anObject];
+    
+    [self updateChangeCount:NSChangeDone];
+}
 @end
